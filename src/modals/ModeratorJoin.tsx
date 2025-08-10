4e7473003612662e5c53cf19258df67c45a303f7
Erfan Mola <hi@erfanxd.ir>
2025-08-10 23:39:58 +0330

    chore: Implement Contest Moderators

---
+++ src/modals/ModeratorJoin.tsx
@@ -0,0 +1,186 @@
+import "./ModeratorJoin.scss";
+import { useNavigate } from "@solidjs/router";
+import {
+	batch,
+	type Component,
+	createMemo,
+	createSignal,
+	onMount,
+	Show,
+} from "solid-js";
+import ContestThumbnail from "../components/ContestThumbnail";
+import CustomMainButton from "../components/CustomMainButton";
+import Modal from "../components/Modal";
+import { SVGSymbol } from "../components/SVG";
+import { useTranslation } from "../contexts/TranslationContext";
+import { requestAPI } from "../utils/api";
+import { modals, setModals } from "../utils/modal";
+import { toggleSignal } from "../utils/signals";
+import type { Contest } from "../utils/store";
+import { getSymbolSVGString } from "../utils/symbols";
+import {
+	invokeHapticFeedbackImpact,
+	invokeHapticFeedbackNotification,
+} from "../utils/telegram";
+import {
+	ContestThemeBackdrops,
+	type ContestThemeSymbol,
+} from "../utils/themes";
+
+const ModalModeratorJoin: Component = () => {
+	const { t } = useTranslation();
+	const navigate = useNavigate();
+
+	const [processing, setProcessing] = createSignal(false);
+	const [data, setData] = createSignal<
+		| undefined
+		| {
+				contest: Pick<
+					Contest,
+					"title" | "image" | "theme" | "verified" | "slug"
+				>;
+		  }
+	>(undefined);
+
+	onMount(async () => {
+		if (!modals.moderatorJoin.slug_moderator) {
+			onClose();
+			return;
+		}
+
+		if (!data()) {
+			await fetchData();
+		}
+	});
+
+	const fetchData = async () => {
+		const request = await requestAPI(
+			`/contest/${modals.moderatorJoin.slug_moderator}/moderators/info`,
+			{},
+			"GET",
+		);
+
+		if (request) {
+			const { result, status } = request;
+
+			if (status === "success") {
+				setData({
+					contest: result.contest,
+				});
+			} else {
+				onClose();
+			}
+		} else {
+			onClose();
+		}
+	};
+
+	const onClose = () => {
+		batch(() => {
+			setModals("moderatorJoin", "slug_moderator", undefined);
+			setModals("moderatorJoin", "open", false);
+		});
+	};
+
+	const onClickJoin = async () => {
+		if (processing()) return;
+		invokeHapticFeedbackImpact("light");
+		setProcessing(true);
+
+		const request = await requestAPI(
+			`/contest/${modals.moderatorJoin.slug_moderator}/moderators/join`,
+			{},
+			"POST",
+		);
+
+		if (request) {
+			const { status } = request;
+
+			if (status === "success") {
+				onClose();
+				toggleSignal("fetchMyContests");
+				invokeHapticFeedbackNotification("success");
+				navigate(`/contest/${data()!.contest.slug}`, {
+					replace: true,
+				});
+			}
+		}
+
+		setProcessing(false);
+	};
+
+	const theme = createMemo(() => {
+		if (!data()?.contest?.theme) return;
+
+		const backdrop = ContestThemeBackdrops.find(
+			(i) => i.id === data()!.contest?.theme?.backdrop,
+		);
+		if (!backdrop) return;
+
+		const symbol: ContestThemeSymbol = {
+			id: data()!.contest.theme!.symbol!,
+			component: getSymbolSVGString(data()!.contest.theme!.symbol!),
+		};
+
+		return {
+			backdrop,
+			symbol,
+		};
+	});
+
+	return (
+		<Modal
+			containerClass="container-modal-moderator-join"
+			class="modal-moderator-join"
+			onClose={onClose}
+			portalParent={document.querySelector("#modals")!}
+		>
+			<Show
+				when={data()}
+				fallback={
+					<div id="container-modal-moderator-join-shimmer">
+						<header>
+							<div class="shimmer"></div>
+							<span class="shimmer"></span>
+						</header>
+
+						<div>
+							<div class="shimmer"></div>
+						</div>
+
+						<div class="shimmer"></div>
+					</div>
+				}
+			>
+				<div>
+					<header>
+						<ContestThumbnail
+							image={data()!.contest?.image}
+							backdrop={theme()?.backdrop}
+							symbol={theme()?.symbol}
+							symbolSize={48}
+						/>
+
+						<h1>
+							{data()!.contest?.title}
+							<Show when={data()!.contest?.verified}>
+								<SVGSymbol id="VsVerifiedFilled" />
+							</Show>
+						</h1>
+					</header>
+
+					<p>{t("modals.moderators.join.prompt")}</p>
+
+					<CustomMainButton
+						text={t("modals.moderators.join.button")}
+						onClick={onClickJoin}
+						disabled={processing()}
+						loading={processing()}
+					/>
+				</div>
+			</Show>
+		</Modal>
+	);
+};
+
+export default ModalModeratorJoin;
